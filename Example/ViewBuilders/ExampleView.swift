b98f7ee77f617b27ef845c79ed9df875a2d32cd4
Lucas Abijmil <lucas.abijmil@gmail.com>
2021-12-14 21:58:22 +0100

    Update Example app & file architecture

--- Example/ViewBuilders/ExampleView.swift
+++ Example/ViewBuilders/ExampleView.swift
@@ -8,87 +8,90 @@ struct ExampleView: View {
 
   var body: some View {
     NavigationView {
-      List(Sample.allCases, rowContent: \.content)
-        .navigationTitle("ViewBuilders Example")
+      List {
+        Section {
+          ForEach(Sample.EnvironmentValue.allCases, content: \.content)
+        } header: {
+          Text("Environment Value Samples")
+            .fontWeight(.bold)
+        }
+
+        Section {
+          ForEach(Sample.ViewModifiers.allCases, content: \.content)
+        } header: {
+          Text("View Modifier Samples")
+            .fontWeight(.bold)
+        }
+      }
+      .navigationTitle("ViewBuilders Example")
     }
   }
 }
 
-enum Sample: String, Identifiable, CaseIterable {
-  case hapticFeedback = "haptic feedback"
-  case interactiveSheet = "interactive sheet"
-  case halfSheet = "half sheet"
-  case hudBool = "HUD Binding<Bool>"
-  case hudItem = "HUD Binding<Item?>"
-  case toasterBoolViewBuilder = "toaster Binding<Bool> with one @ViewBuilder"
-  case toasterBoolViewBuilders = "toaster Binding<Bool> with two @ViewBuilder"
-  case toasterItemViewBuilder = "toaster Binding<Item?> with one @ViewBuilder"
-  case toasterItemViewBuilders = "toaster Binding<Item?> with two @ViewBuilder"
-  case navigationAppearance = "navigation bar appearance"
-  case navigationAction = "navigation action"
-  case dialogBool = "dialog Binding<Bool>"
-  case dialogItem = "dialog Binding<Item?>"
-  case currentDevice = "current device"
+enum Sample {
 
-  var id: String { rawValue }
+  enum EnvironmentValue: String, Identifiable, CaseIterable {
+    case hapticFeedback = "haptic feedback"
+    case navigationAction = "navigation action"
+    case currentDevice = "current device"
 
-  @ViewBuilder var content: some View {
-    switch self {
-    case .hapticFeedback:
+    var id: String { rawValue }
+
+    @ViewBuilder var content: some View {
       NavigationLink(rawValue.firstLetterCapitalized) {
-        HapticFeedbackSample()
+        switch self {
+        case .hapticFeedback:
+          HapticFeedbackSample()
+        case .navigationAction:
+          NavigationActionSample()
+        case .currentDevice:
+          CurrentDeviceSample()
+        }
       }
-    case .interactiveSheet:
+    }
+  }
+
+  enum ViewModifiers: String, Identifiable, CaseIterable {
+    case interactiveSheet = "interactive sheet"
+    case halfSheet = "half sheet"
+    case hudBool = "HUD Binding<Bool>"
+    case hudItem = "HUD Binding<Item?>"
+    case toasterBoolViewBuilder = "toaster Binding<Bool> with one @ViewBuilder"
+    case toasterBoolViewBuilders = "toaster Binding<Bool> with two @ViewBuilder"
+    case toasterItemViewBuilder = "toaster Binding<Item?> with one @ViewBuilder"
+    case toasterItemViewBuilders = "toaster Binding<Item?> with two @ViewBuilder"
+    case navigationAppearance = "navigation bar appearance"
+    case dialogBool = "dialog Binding<Bool>"
+    case dialogItem = "dialog Binding<Item?>"
+
+    var id: String { rawValue }
+
+    @ViewBuilder var content: some View {
       NavigationLink(rawValue.firstLetterCapitalized) {
-        InteractiveSheetSample()
-      }
-    case .halfSheet:
-      NavigationLink(rawValue.firstLetterCapitalized) {
-        HalfSheetSample()
-      }
-    case .hudBool:
-      NavigationLink(rawValue.firstLetterCapitalized) {
-        HUDBoolSample()
-      }
-    case .hudItem:
-      NavigationLink(rawValue.firstLetterCapitalized) {
-        HUDItemSample()
-      }
-    case .toasterBoolViewBuilder:
-      NavigationLink(rawValue.firstLetterCapitalized) {
-        ToasterBoolViewBuilderSample()
-      }
-    case .toasterBoolViewBuilders:
-      NavigationLink(rawValue.firstLetterCapitalized) {
-        ToasterBoolViewBuildersSample()
-      }
-    case .toasterItemViewBuilder:
-      NavigationLink(rawValue.firstLetterCapitalized) {
-        ToasterItemViewBuilderSample()
-      }
-    case .toasterItemViewBuilders:
-      NavigationLink(rawValue.firstLetterCapitalized) {
-        ToasterItemViewBuildersSample()
-      }
-    case .navigationAppearance:
-      NavigationLink(rawValue.firstLetterCapitalized) {
-        NavigationAppearanceSample()
-      }
-    case .navigationAction:
-      NavigationLink(rawValue.firstLetterCapitalized) {
-        NavigationActionSample()
-      }
-    case .dialogBool:
-      NavigationLink(rawValue.firstLetterCapitalized) {
-        DialogBoolSample()
-      }
-    case .dialogItem:
-      NavigationLink(rawValue.firstLetterCapitalized) {
-        DialogItemSample()
-      }
-    case .currentDevice:
-      NavigationLink(rawValue.firstLetterCapitalized) {
-        CurrentDeviceSample()
+        switch self {
+        case .interactiveSheet:
+          InteractiveSheetSample()
+        case .halfSheet:
+          HalfSheetSample()
+        case .hudBool:
+          HUDBoolSample()
+        case .hudItem:
+          HUDItemSample()
+        case .toasterBoolViewBuilder:
+          ToasterBoolViewBuilderSample()
+        case .toasterBoolViewBuilders:
+          ToasterBoolViewBuildersSample()
+        case .toasterItemViewBuilder:
+          ToasterItemViewBuilderSample()
+        case .toasterItemViewBuilders:
+          ToasterItemViewBuildersSample()
+        case .navigationAppearance:
+          NavigationAppearanceSample()
+        case .dialogBool:
+          DialogBoolSample()
+        case .dialogItem:
+          DialogItemSample()
+        }
       }
     }
   }
